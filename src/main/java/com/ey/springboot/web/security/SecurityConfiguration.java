7df57651fc799a701068309415606678007c6a31
Brian <test@test.com>
2020-03-03 08:58:34 -0500

    Add security debug flag

--- src/main/java/com/ey/springboot/web/security/SecurityConfiguration.java
+++ src/main/java/com/ey/springboot/web/security/SecurityConfiguration.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.DefaultRedirectStrategy;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 
 @Configuration
+@EnableWebSecurity(debug = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 	//Create User - ey/dummy
 	@Autowired
