12c90edab3664c428be6cdd436c93a080a003843
Brian <test@test.com>
2020-03-03 09:06:46 -0500

    Modify antMatcher pattern on Spring SecurityConfiguration

--- src/main/java/com/ey/springboot/web/security/SecurityConfiguration.java
+++ src/main/java/com/ey/springboot/web/security/SecurityConfiguration.java
@@ -23,8 +23,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 	@Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/login").permitAll()
-                //.antMatchers("/", "/*todo*/**").access("hasRole('USER')").and()
-                .antMatchers("/**").access("hasRole('USER')").and()
+                .antMatchers("/**").hasRole("USER").and()
                 .formLogin()
                 .successForwardUrl("/home");
     }
